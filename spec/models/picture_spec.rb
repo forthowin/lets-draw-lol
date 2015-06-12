ff2840009c59b470436364c3e78a6516df336f7a
Tho Nguyen <thogotmail@gmail.com>
2015-06-11 23:19:36 -0500

    test for picture and category model

---
+++ spec/models/picture_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Picture do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:category_id) }
+  it { should validate_presence_of(:image) }
+  it { should belong_to(:category) }
+end
