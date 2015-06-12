ab292c4a1d5e6ccb79a170ffc89b8a87dd97df04
Tho Nguyen <thogotmail@gmail.com>
2015-06-12 18:31:37 -0500

    add timer and draw on canvas

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,11 @@
 class PicturesController < ApplicationController
   def draw_champion
+  end
+
+  def start_draw
     @champion = Category.where(name: 'Champions').first.pictures.sample
+    respond_to do |format|
+      format.js
+    end
   end
 end
