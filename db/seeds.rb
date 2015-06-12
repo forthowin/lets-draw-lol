4664eadb21709227cf30d01df060fd6547499f49
Tho Nguyen <thogotmail@gmail.com>
2015-06-11 23:04:48 -0500

    seed pictures with champion splash art

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+
+champ_cat = Category.create(name: "Champions")
+
+response = JSON.parse(open("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=#{ENV['RIOT_API_KEY']}").read)
+
+response["data"].keys.each do |name|
+  Picture.create(name: name, image: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/#{name}_0.jpg", category: champ_cat)
+end
