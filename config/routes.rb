66b743c02f1dbcb2f01550017222084e4da57ee9
Tho Nguyen <thogotmail@gmail.com>
2015-06-13 16:35:55 -0500

    add route for drawing pros

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#front'
   get '/draw/champions', to: 'drawings#draw_champion'
   post '/draw/champions', to: 'drawings#start_draw'
+  get '/draw/pros', to: 'drawings#draw_pros'
   resources :drawings, only: [:create, :index]
 end
