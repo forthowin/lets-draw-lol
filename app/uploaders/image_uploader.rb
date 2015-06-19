67026ee85bbe852c74b31e9d9984051e63f88aa0
Tho Nguyen <thogotmail@gmail.com>
2015-06-19 01:02:43 -0500

    resize image for thumbnail

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-  
+  include CarrierWave::MiniMagick
+
+  version :thumb do
+    process resize_to_fill: [200,168]
+  end
 end
