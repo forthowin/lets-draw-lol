50b92590041d086999a4493dc06c5f0d4fd8e50e
Tho Nguyen <thogotmail@gmail.com>
2015-06-26 15:27:46 -0500

    test for comments association

--- spec/models/drawing_spec.rb
+++ spec/models/drawing_spec.rb
@@ -5,4 +5,5 @@ describe Drawing do
   it { should belong_to :category }
   it { should belong_to :user }
   it { should have_many :likes }
+  it { should have_many(:comments).order("created_at DESC")}
 end
