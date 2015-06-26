5411e3376c130a88474147671fe6d0b4cb5b0ff7
Tho Nguyen <thogotmail@gmail.com>
2015-06-25 20:08:07 -0500

    create show action

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,6 +1,7 @@
 class Users::UsersController < ApplicationController
   def show
     @user = User.find params[:id]
-    @drawings = @user.drawings.page(params[:page])
+    @drawings = @user.drawings.page(params[:drawings_page]).per(20)
+    @like_drawings = Drawing.joins(:likes).where("likes.user_id = ?", @user.id).page(params[:like_drawings_page]).per(20)
   end
 end
