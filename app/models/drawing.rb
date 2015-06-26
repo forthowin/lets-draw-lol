ca4ce12fe00d9a0f97185bf1a12acb5a89f14df3
Tho Nguyen <thogotmail@gmail.com>
2015-06-26 15:10:30 -0500

    add has many comments ordered by created at DESC

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -3,6 +3,7 @@ class Drawing < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :likes
+  has_many :comments, -> { order("created_at DESC") }
 
   mount_uploader :image, ImageUploader
 end
