4acd143fe3265280cea342c4ced49067c2e8bd47
Tho Nguyen <thogotmail@gmail.com>
2015-06-25 21:00:14 -0500

    add test for association with likes

--- spec/models/drawing_spec.rb
+++ spec/models/drawing_spec.rb
@@ -4,4 +4,5 @@ describe Drawing do
   it { should belong_to :picture }
   it { should belong_to :category }
   it { should belong_to :user }
+  it { should have_many :likes }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should validate_length_of(:password).is_at_least(5) }
   it { should have_many(:drawings).order("created_at DESC") }
+  it { should have_many :likes }
 end
