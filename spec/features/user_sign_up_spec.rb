9c49ec20b94c9f11726bfe4708c3092cbee42ad1
Tho Nguyen <thogotmail@gmail.com>
2015-07-01 13:39:16 -0500

    removed context

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
 feature 'user signs up', :js do
-  context 'with valid inputs' do
-    given(:username) { 'forthowin' }
-    given(:email) { 'forthowin@gmail.com' }
-    given(:password) { 'password' }
+  given(:username) { 'forthowin' }
+  given(:email) { 'forthowin@gmail.com' }
+  given(:password) { 'password' }
 
-    scenario 'the user signs in at the front page' do
-      visit root_path
-      click_link 'REGISTERING'
+  scenario 'the user signs in at the front page with valid inputs' do
+    visit root_path
+    click_link 'REGISTERING'
+    fill_in_sign_up_form_with_valid_input
+    expect(page).to have_content 'forthowin'
+  end
+
+  scenario 'the user signs at the gallery page with valid inputs' do
+    visit drawings_path
+    expect(page).to have_content 'Gallery'
+    click_link 'Login/SignUp'
+    within('.left-panel') do
       fill_in_sign_up_form_with_valid_input
-      expect(page).to have_content 'forthowin'
     end
-
-    scenario 'the user signs at the gallery page' do
-      visit drawings_path
-      expect(page).to have_content 'Gallery'
-      click_link 'Login/SignUp'
-      within('.left-panel') do
-        fill_in_sign_up_form_with_valid_input
-      end
-      expect(page).to have_content 'forthowin'
-      expect(page).to have_content 'Gallery'
-    end
+    expect(page).to have_content 'forthowin'
+    expect(page).to have_content 'Gallery'
   end
 
   def fill_in_sign_up_form_with_valid_input
