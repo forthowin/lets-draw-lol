ff615597909a294e2ef85a79be3f87a0c14883c7
Tho Nguyen <thogotmail@gmail.com>
2015-07-01 13:20:32 -0500

    feature test for user sign up with correct info

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 feature 'user signs up', :js do
-  scenario 'with valid inputs' do
-    visit root_path
-    click_link 'REGISTERING'
-    fill_in 'Username', with: 'forthowin'
-    fill_in 'Email', with: 'forthowin@gmail.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_button 'Sign up'
-    expect(page).to have_content 'forthowin'
+  context 'with valid inputs' do
+    scenario 'the user logs in at the front page' do
+      visit root_path
+      click_link 'REGISTERING'
+      fill_in 'Username', with: 'forthowin'
+      fill_in 'Email', with: 'forthowin@gmail.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'Sign up'
+      expect(page).to have_content 'forthowin'
+    end
+
+    scenario 'the user logs in at the gallery page' do
+      visit drawings_path
+      expect(page).to have_content 'Gallery'
+      click_link 'Login/SignUp'
+      within('.left-panel') do
+        fill_in 'Username', with: 'forthowin'
+        fill_in 'Email', with: 'forthowin@gmail.com'
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button 'Sign up'
+      end
+      expect(page).to have_content 'forthowin'
+      expect(page).to have_content 'Gallery'
+    end
   end
+
 end
