cbd4b5497beba5c99407bbc288e957a10a3cb555
Tho Nguyen <thogotmail@gmail.com>
2015-07-01 13:30:01 -0500

    refactor

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 feature 'user signs up', :js do
   context 'with valid inputs' do
+    given(:username) { 'forthowin' }
+    given(:email) { 'forthowin@gmail.com' }
+    given(:password) { 'password' }
+
     scenario 'the user logs in at the front page' do
       visit root_path
       click_link 'REGISTERING'
-      fill_in 'Username', with: 'forthowin'
-      fill_in 'Email', with: 'forthowin@gmail.com'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button 'Sign up'
+      fill_in_sign_up_form_with_valid_input
       expect(page).to have_content 'forthowin'
     end
 
@@ -18,15 +18,19 @@ feature 'user signs up', :js do
       expect(page).to have_content 'Gallery'
       click_link 'Login/SignUp'
       within('.left-panel') do
-        fill_in 'Username', with: 'forthowin'
-        fill_in 'Email', with: 'forthowin@gmail.com'
-        fill_in 'Password', with: 'password'
-        fill_in 'Password confirmation', with: 'password'
-        click_button 'Sign up'
+        fill_in_sign_up_form_with_valid_input
       end
       expect(page).to have_content 'forthowin'
       expect(page).to have_content 'Gallery'
     end
+  end
+
+  def fill_in_sign_up_form_with_valid_input
+    fill_in 'Username', with: username
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_button 'Sign up'
   end
 
 end
