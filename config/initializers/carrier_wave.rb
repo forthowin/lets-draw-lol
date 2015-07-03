ac8a3fb7249bfd12cbf2158a7f62a025d920cef3
Tho Nguyen <thogotmail@gmail.com>
2015-07-02 22:58:48 -0500

    change path for test images to be in public folder

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -22,11 +22,11 @@ if Rails.env.test?
     next if klass.anonymous?
     klass.class_eval do
       def cache_dir
-        "#{Rails.root}/spec/support/uploads/tmp"
+        "#{Rails.root}/public/spec/support/uploads/tmp"
       end
 
       def store_dir
-        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        "#{Rails.root}/public/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
       end
     end
   end
